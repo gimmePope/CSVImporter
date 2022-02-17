b902dd645becf692c0afe30d7e43c1afd95e601a
Otulana Olujimi <jimiotulana@gmail.com>
2022-02-17 14:46:12 +0100

    Add processUploadCSV method to the UploadController class, intended to handle csv uploaded file submission

--- src/main/java/com/pocosoft/csvImport/frontend/UploadController.java
+++ src/main/java/com/pocosoft/csvImport/frontend/UploadController.java
@@ -3,6 +3,7 @@ package com.pocosoft.csvImport.frontend;
 import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UploadController {
@@ -21,6 +22,12 @@ public class UploadController {
 	{
 		//System.out.println("I JUST WANT TO LET U KNOW I HEAR YOU FROM UPLOAD !!!!!!");
 		return "index";
+	}
+	
+	@PostMapping("/process/csv/upload")
+	public String  processUploadedCSV()
+	{
+		return "";
 	}
 
 }
