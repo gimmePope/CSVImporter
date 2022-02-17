5deb8bcd45df8b00f7fb69c30bac930af36b4b14
Otulana Olujimi <jimiotulana@gmail.com>
2022-02-17 13:59:54 +0100

    Set up project to upload csv and import to database, only the web front end available at this point

---
+++ src/main/java/com/pocosoft/csvImport/frontend/UploadController.java
@@ -0,0 +1,26 @@
+package com.pocosoft.csvImport.frontend;
+
+import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UploadController {
+	
+	
+	@GetMapping("/")
+	public String indexPage()
+	{
+		System.out.println("I JUST WANT TO LET U KNOW I HEAR YOU!!!!!!");
+		
+		return "index";
+	}
+	
+	@GetMapping("/upload")
+	public String displayUpload()
+	{
+		System.out.println("I JUST WANT TO LET U KNOW I HEAR YOU FROM UPLOAD !!!!!!");
+		return "index";
+	}
+
+}
