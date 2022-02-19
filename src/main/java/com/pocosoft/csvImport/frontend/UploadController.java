8a2083fc130a45f56d0f3e334ab55d7d6829822a
Otulana Olujimi <jimiotulana@gmail.com>
2022-02-19 12:24:48 +0100

    Add thyme leaf templating to the index page, and pass uploaded file as parameter to the upload controller

--- src/main/java/com/pocosoft/csvImport/frontend/UploadController.java
+++ src/main/java/com/pocosoft/csvImport/frontend/UploadController.java
@@ -1,9 +1,18 @@
 package com.pocosoft.csvImport.frontend;
 
+import java.io.IOException;
+
+import javax.batch.operations.JobRestartException;
+
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UploadController {
@@ -25,7 +34,7 @@ public class UploadController {
 	}
 	
 	@PostMapping("/process/csv/upload")
-	public String  processUploadedCSV()
+	public String  processUploadedCSV(@RequestParam("csv_file") MultipartFile multipartFile)throws IOException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException
 	{
 		return "";
 	}
